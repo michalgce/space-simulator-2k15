0e43c0f42719839153b51b45736ff23f4ee69a9a
Maroo <maroo672@hotmail.com>
2015-03-26 16:25:11 +0100

    Szablon sterowania

---
+++ Assets/Skrypty/Sterowanie/Statek/RuchObrotowyStatku.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Naped obracajacy statkiem wzdloz osi X, Y i Z (odpowiedniki wolantu i pedalow)
+
+public class RuchObrotowyStatku : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Skrypty/Sterowanie/Statek/RuchPostepowyStatku.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Naped postepowy, przede wszystkim naped przedni i wsteczny. 
+// Dla statku kosmicznego przydalby sie jeszcze ciag gora dol i na boki
+
+public class RuchPostepowyStatku : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
