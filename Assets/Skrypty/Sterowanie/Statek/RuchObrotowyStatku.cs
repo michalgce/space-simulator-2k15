d078519533cdc4448c7eec7b03985dd4d2db6ebc
Maroo <maroo672@hotmail.com>
2015-03-26 19:11:13 +0100

    Poprawka emitera, ciag przedniego

--- Assets/Skrypty/Sterowanie/Statek/RuchObrotowyStatku.cs
+++ Assets/Skrypty/Sterowanie/Statek/RuchObrotowyStatku.cs
@@ -5,13 +5,24 @@ using System.Collections;
 
 public class RuchObrotowyStatku : MonoBehaviour {
 
-	// Use this for initialization
+	public float czuloscMyszyX = 5.0f;
+	public float czuloscMyszyY = 5.0f;
+	
+	float rotacjaY = 0.0f;
+
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		
+		if (Input.GetMouseButton(1)) {
+
+			float rotacjaX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * czuloscMyszyX;
+			rotacjaY += Input.GetAxis ("Mouse Y") * czuloscMyszyY;
+			rotacjaY = Mathf.Clamp (rotacjaY, -89.5f, 89.5f);
+			transform.localEulerAngles = new Vector3 (-rotacjaY, rotacjaX, 0.0f);
+
+		}
 	}
 }
--- Assets/Skrypty/Sterowanie/Statek/RuchPostepowyStatku.cs
+++ Assets/Skrypty/Sterowanie/Statek/RuchPostepowyStatku.cs
@@ -6,13 +6,69 @@ using System.Collections;
 
 public class RuchPostepowyStatku : MonoBehaviour {
 
-	// Use this for initialization
+
+	private int mocSilnika = 0;
+
+	private int mnoznikMocySilnika = 1;
+
+	public ParticleSystem spalinySilnikaGlownego;
+
+	public float x;
+
+	public float y;
+
+	public float z;
+
+	public float q;
+
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+
+		// przod/tyl
+
+		this.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, mocSilnika * Time.deltaTime));
+		//this.gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.rotation.eulerAngles.normalized * mocSilnika * Time.deltaTime);
+
+
+
+		// DO PORPAWKI
+
+		if (Input.anyKeyDown) {
+
+			if (Input.GetKeyDown (KeyCode.Alpha0))
+				mocSilnika = 0;
+			if (Input.GetKeyDown (KeyCode.Alpha1))
+				mocSilnika = 1 * mnoznikMocySilnika;
+			if (Input.GetKeyDown (KeyCode.Alpha2))
+				mocSilnika = 2 * mnoznikMocySilnika;
+			if (Input.GetKeyDown (KeyCode.Alpha3))
+				mocSilnika = 3 * mnoznikMocySilnika;
+			if (Input.GetKeyDown (KeyCode.Alpha4))
+				mocSilnika = 4 * mnoznikMocySilnika;
+			if (Input.GetKeyDown (KeyCode.Alpha5))
+				mocSilnika = 5 * mnoznikMocySilnika;
+
+
+			spalinySilnikaGlownego.startSpeed = 0.2f + mocSilnika / 2.0f;
+			spalinySilnikaGlownego.startSize = 0.5f + mocSilnika / 10.0f;
+			spalinySilnikaGlownego.emissionRate = 2 + mocSilnika * 5;
+		}
+
+		if (!Input.GetMouseButton (0)) {
+			// RUCHY WSAD
+		}
+		
+
+		// reset
+		if (Input.GetKey (KeyCode.R)) {
+			gameObject.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+			gameObject.transform.rotation = new Quaternion (0.0f, 0.0f, 0.0f, 0.0f);
+		}
+
+
 	}
 }
