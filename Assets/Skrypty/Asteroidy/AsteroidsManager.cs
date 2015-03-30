8b529f3463a139172070b38b697e3bd2827fd404
jacek <jaculo@gmail.com>
2015-03-30 23:04:32 +0200

    #414378 - dodanie asteroid

---
+++ Assets/Skrypty/Asteroidy/AsteroidsManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidsManager : MonoBehaviour
+{
+	public bool czyGenerowac = false;
+	public float maxDistance = 10;
+	private GameObject[] prefabs = new GameObject[3];
+	private GameObject statek;
+	private List<GameObject> asteroids = new List<GameObject>();
+	private Vector3 statekLoc;
+	private float generujCo = 1; //sekundy
+	private float kasujCo = 3; //sekundy
+
+	void Start ()
+	{
+		statek = GameObject.FindGameObjectWithTag ("Statek");
+		statekLoc = statek.transform.position;
+		prefabs [0] = (GameObject) Resources.Load("Asteroidy/AsteroidaA");
+		prefabs [1] = (GameObject) Resources.Load("Asteroidy/AsteroidaB");
+		prefabs [2] = (GameObject) Resources.Load("Asteroidy/AsteroidaC");
+		StartCoroutine (SpawnAsteroids ());
+		StartCoroutine (DestroyAsteroids ());
+	}
+	
+	void Update ()
+	{
+		statekLoc = statek.transform.position;
+	}
+	
+	IEnumerator SpawnAsteroids ()
+	{
+		while (czyGenerowac)
+		{
+			GameObject asteroid = prefabs [Random.Range (0, prefabs.Length)];
+			Vector3 spawnPosition = new Vector3 (statekLoc.x+1, statekLoc.y+1, statekLoc.z+1); //TODO: do zrobienia generowanie dookoła? statku
+			Quaternion spawnRotation = Quaternion.identity;
+			GameObject asteroidInstantiated = (GameObject) Instantiate (asteroid, spawnPosition, spawnRotation);
+			asteroids.Add(asteroidInstantiated);
+			yield return new WaitForSeconds (generujCo);
+		}
+	}
+
+	IEnumerator DestroyAsteroids ()
+	{
+		while (czyGenerowac)
+		{
+			List<GameObject> asteroidsToDestroy = new List<GameObject>();
+			foreach (GameObject go in asteroids) 
+			{
+				float distance = Vector3.Distance(statekLoc, go.transform.position);
+				if(distance > maxDistance){
+					asteroidsToDestroy.Add(go);
+				}
+			}
+			foreach (GameObject go in asteroidsToDestroy) 
+			{
+				asteroids.Remove(go);
+				Destroy(go);
+			}
+			yield return new WaitForSeconds (kasujCo);
+		}
+	}
+
+}
+
---
+++ Assets/Skrypty/Asteroidy/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+
+	private Vector3[] vectors = new Vector3[]{new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(1, 1, 1), 
+		new Vector3(1, 0, 1), new Vector3(1, 1, 0), new Vector3(1, 0, 0), new Vector3(0, 1, 0)};
+
+	void Start ()
+	{
+		int rand = (int) Random.Range (0, 6);
+		GetComponent<Rigidbody>().velocity = vectors[rand] * speed;
+	}
+}
