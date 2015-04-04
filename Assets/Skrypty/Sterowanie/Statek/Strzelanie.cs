de31fe89f43d9ed5be7413006a6d561bebfcd6d7
Michał Witkowski <michalgce@gmail.com>
2015-04-04 19:55:06 +0200

    Dodano strzelanie, na przycisku Fire1.

---
+++ Assets/Skrypty/Sterowanie/Statek/Strzelanie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Strzelanie : MonoBehaviour {
+
+	public GameObject strzal;
+	public Transform spawnPocisku;
+	public float fireRate;
+	public int destroyAfterSec;
+
+	private float nextFire;
+
+	void Update () {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			GameObject objekt = Instantiate(strzal, spawnPocisku.position, spawnPocisku.rotation) as GameObject;
+			Destroy(objekt, destroyAfterSec);
+		}
+	}
+}
---
+++ Assets/Skrypty/Strzal/Strzal.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Strzal : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+		GameObject statek = GameObject.FindGameObjectWithTag ("Statek");
+		GetComponent<Rigidbody>().velocity = statek.transform.forward * speed;
+	}
+}
