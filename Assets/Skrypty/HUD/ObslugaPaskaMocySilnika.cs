c1e0f04733a52c07173e602958bc721b83c92417
Maroo <maroo672@hotmail.com>
2015-05-27 13:58:57 +0200

    Naprawa Celownika, wskazniki predkosci i mocy silnika

---
+++ Assets/Skrypty/HUD/ObslugaPaskaMocySilnika.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObslugaPaskaMocySilnika : MonoBehaviour {
+
+	public RectTransform pasekPrzewijalny;
+	public GameObject statek;
+
+	private RuchPostepowyStatku ruchPostepowyStatku;
+
+	private float pozycjaX;
+	
+	private float pozycjaStartowaY;
+	private float dlugoscPaska;
+
+	// Use this for initialization
+	void Start () {
+		pozycjaX = pasekPrzewijalny.position.x;
+		pozycjaStartowaY = pasekPrzewijalny.position.y;
+		dlugoscPaska = pasekPrzewijalny.rect.height;
+		ruchPostepowyStatku = statek.GetComponent<RuchPostepowyStatku>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//pasekPrzewijalny.position = new Vector3 (pozycjaStartowaX - (1.0f - stanPaska) * szerokoscPaska, pozycjaY, 0.0f);
+		pasekPrzewijalny.position = new Vector3 (pozycjaX, pozycjaStartowaY + (ruchPostepowyStatku.mocSilnikaGlownego) * dlugoscPaska, 0.0f);
+
+	}
+}
---
+++ Assets/Skrypty/HUD/ObslugaWskaznikaPredkosci.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObslugaWskaznikaPredkosci : MonoBehaviour {
+
+
+	public GameObject statek;
+	public Text tekst;
+
+	private RuchPostepowyStatku ruchPostepowyStatku;
+
+	//private 
+
+	// Use this for initialization
+	void Start () {
+		ruchPostepowyStatku = statek.GetComponent<RuchPostepowyStatku> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float szybkosc = - 270.0f * ruchPostepowyStatku.predkoscStatku / ruchPostepowyStatku.predkoscStatkuMax - 8.0f;
+		this.transform.eulerAngles = new Vector3(0.0f, 0.0f, szybkosc);
+		tekst.text = ruchPostepowyStatku.predkoscStatku.ToString("f");
+}
+}
