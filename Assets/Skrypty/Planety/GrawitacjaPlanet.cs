11bfc0f68d91b803263a98f2616bd12f13e7b2a3
Maroo <maroo672@hotmail.com>
2015-05-28 20:04:25 +0200

    Dodana grawitacja do planet przyciagajaca statek

---
+++ Assets/Skrypty/Planety/GrawitacjaPlanet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrawitacjaPlanet : MonoBehaviour {
+
+	public GameObject statek;
+	public float mnoznikSilyGrawitacji;
+
+	private GameObject[] planety;
+
+	// Use this for initialization
+	void Start () {
+		planety = GameObject.FindGameObjectsWithTag ("Planeta");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		string text = "Grawitacje:  ";
+
+		foreach (GameObject planeta in planety) {
+			Vector3 wektor = planeta.transform.position - statek.transform.position;
+			float silaPrzyciagania = planeta.transform.localScale.x * mnoznikSilyGrawitacji / wektor.magnitude / wektor.magnitude;
+			if (silaPrzyciagania > statek.GetComponent<RuchPostepowyStatku>().predkoscStatkuMax)
+				silaPrzyciagania = statek.GetComponent<RuchPostepowyStatku>().predkoscStatkuMax;
+			statek.GetComponent<Rigidbody>().AddForce(wektor.normalized * silaPrzyciagania);
+			text += ">" + planeta.name + ": " + silaPrzyciagania + ";  ";
+		}
+		Debug.Log (text);
+	}
+}
